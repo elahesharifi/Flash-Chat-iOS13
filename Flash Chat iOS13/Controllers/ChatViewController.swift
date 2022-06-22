b4287c5fd34fb85554e6dab381f59a8cbe42a19e
elahe <el.sh192014@gmail.com>
2022-06-23 00:54:16 +0200

    add imageView at left of cell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -96,7 +96,6 @@ extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        
         cell.label.text = messages[indexPath.row].body
         return cell
     }
